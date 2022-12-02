49394131d61c6b043996abfd09874e9c7f2de3ca
AntoineR <antoine.roubaud@epita.frgit config --global user.email antoine.roubaud@epita.fr>
2022-12-02 16:09:00 +0100

    Complete game function, need to update when there is the same color to not print x times that she is present

--- mastermind/src/mastermind.c
+++ mastermind/src/mastermind.c
@@ -58,54 +58,75 @@ void play_mastermind(struct secret_code sc, struct secret_code pc) {
     bool is_color4_present = false;
 
     //Check color 1
-    if (pc.color1 == sc.color1)
-        is_color1_good = true;
+    is_color1_present = pc.color1 == sc.color2 ||
+                        pc.color1 == sc.color3 ||
+                        pc.color1 == sc.color4;
 
-    if (pc.color1 == sc.color2)
-        is_color1_present = true;
-
-    if (pc.color1 == sc.color3)
-        is_color1_present = true;
-
-    if (pc.color1 == sc.color4)
-        is_color1_present = true;
+    is_color1_good = pc.color1 == sc.color1;
 
     //Check color 2
-    if (pc.color2 == sc.color1)
-        is_color2_present = true;
+    is_color2_present = pc.color2 == sc.color1 ||
+                        pc.color2 == sc.color3 ||
+                        pc.color2 == sc.color4;
 
-    if (pc.color2 == sc.color2)
-        is_color2_good = true;
-
-    if (pc.color2 == sc.color3)
-        is_color2_present = true;
-
-    if (pc.color2 == sc.color4)
-        is_color2_present = true;
+    is_color2_good = pc.color2 == sc.color2;
 
     //Check color 3
-    if (pc.color3 == sc.color1)
-        is_color3_present = true;
+    is_color3_present = pc.color3 == sc.color1 ||
+                        pc.color3 == sc.color2 ||
+                        pc.color3 == sc.color4;
 
-    if (pc.color3 == sc.color2)
-        is_color3_present = true;
-
-    if (pc.color3 == sc.color3)
-        is_color3_good = true;
-
-    if (pc.color3 == sc.color4)
-        is_color3_present = true;
+    is_color3_good = pc.color3 == sc.color3;
 
     //Check color 4
-    if (pc.color4 == sc.color1)
-        is_color4_present = true;
+    is_color4_present = pc.color4 == sc.color1 ||
+                        pc.color4 == sc.color2 ||
+                        pc.color4 == sc.color3;
 
-    if (pc.color4 == sc.color2)
-        is_color4_present = true;
+    is_color4_good = pc.color4 == sc.color4;
 
-    if (pc.color4 == sc.color3)
-        is_color4_present = true;
+    // Result
+    int color_present = 0;
+    int color_good = 0;
+    
+    if (is_color1_present){
+        color_present += 1;
+    }
 
-    if (pc.color4 == sc.color4)
-        is_color4_good = true;
+    if (is_color2_present){
+        color_present += 1;
+    }
+
+    if (is_color3_present){
+        color_present += 1;
+    }
+
+    if (is_color4_present){
+        color_present += 1;
+    }
+
+
+    if (is_color1_good) {
+        color_good += 1;
+    }
+
+    if (is_color2_good) {
+        color_good += 1;
+    }
+
+    if (is_color3_good) {
+        color_good += 1;
+    }
+
+    if (is_color4_good) {
+        color_good += 1;
+    }
+
+    if (color_good == 4){
+        printf("Congratulation ! You won !\n\n");
+        return;
+    }
+
+    printf("Present color: %d\n", color_present);
+    printf("Well placed color : %d\n", color_good);
 }
